b8edf857a557759d0f4235d6000d50a8d09db44e
VanyaDem <educationvanyadem@gmail.com>
2024-06-01 01:55:55 +0300

    added specific method for getting notes by user

--- src/main/java/com/MyNote/MyNote/model/repository/NoteRepository.java
+++ src/main/java/com/MyNote/MyNote/model/repository/NoteRepository.java
@@ -1,10 +1,16 @@
 package com.MyNote.MyNote.model.repository;
 
 import com.MyNote.MyNote.model.entity.Note;
+import com.MyNote.MyNote.model.entity.NoteUser;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findAllByNoteUser(NoteUser noteUser, Sort sort);
 
 }
