c3341520a5fdf2f7b4b52a6c86e500092dd77c57
VanyaDem <educationvanyadem@gmail.com>
2024-06-01 01:52:24 +0300

    rename User class

--- src/main/java/com/MyNote/MyNote/model/repository/UserRepository.java
+++ src/main/java/com/MyNote/MyNote/model/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.MyNote.MyNote.model.repository;
 
-import com.MyNote.MyNote.model.entity.User;
+import com.MyNote.MyNote.model.entity.NoteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<NoteUser, Integer> {
 
-    Optional<User> findByUsername(String username);
+    Optional<NoteUser> findByUsername(String username);
 }
